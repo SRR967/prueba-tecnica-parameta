f5e709102e943f2456ca6760d5ea0097ef282ff0
Santiago Ramon <santiramon967@gmail.com>
2025-07-16 11:56:53 -0500

    feat: Initial project structure

---
+++ src/main/java/com/parameta/pruebatecnicaparameta/PruebaTecnicaParametaApplication.java
@@ -0,0 +1,13 @@
+package com.parameta.pruebatecnicaparameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaParametaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaParametaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parameta/pruebatecnicaparameta/PruebaTecnicaParametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parameta.pruebatecnicaparameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaParametaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
